2c1ac2ee4967301ab3a74ebf9293c8965a6bb159
leejinhwa <slzp0119@gmail.com>
2020-10-19 16:34:27 +0900

    자바변수

---
+++ java/src/day1/변수.java
@@ -0,0 +1,66 @@
+package day1;
+
+public class 변수 {
+
+	public static void main(String[] args) {
+		// 주석 : 코드의 영향을 미치지 않은 문구로,
+		// 설명을 위해사용
+		// //는 한줄 주석
+		/* 주석이다
+		 * 단체주석임
+		 * 호호호호호호호호호호호*/
+		//변수 선언 방법
+		//변수타입 :변수명;
+		char ch;//문자형 변수 ch를 선언
+		int num1;
+		long num2;
+		float num3;
+		double num4;
+		boolean bool;
+		int appleCount;//변수의 이름은 의미있게 대소문자구분
+	    //int ch;//중복 선언 불가
+		//변수 선언 및 초기화 방법
+		//변수타입 변수명 = 값;
+		//문자는 한글자를의미, 문자는 앞뒤로 ''를 붙임
+		char cha1 = 'A';
+		char cha2 = '1';
+		char cha3 = '한';
+		char cha4 = '\'';
+		char cha5 = '_';
+		
+		int numb1 = 10; // 십진수 10을 저장
+		int numb2 = 010; //8진수 10을 저장 =>10진수 8
+		int numb3 = 0x10;//16진수 10을 저장=>10진수 16
+		// int numb4 = 123456789012;
+		// 큰수의 경우 int형으로 표현할수 없다
+		//long 은 접미사 L을 쓸수있다
+		long numb5 = 123456789012L; //큰수는 마지막에L붙여아됨
+		
+		//실수
+		//기본 실수값은 double형으로 인식이된다.
+		//float형의 실수값은 접미사 F를 꼭 붙인다.
+		float numb6 = 1.234F;
+		
+		double numb7 = 1.236;
+		
+		boolean bool1 = true;
+		boolean bool2 = false;
+		
+		//변수 선언후 초기화2
+		// 변수타입 변수명 = 변수명;
+		int kor1 = 100;
+		int kor2 = kor1;
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+
+	}
+
+}
---
+++ java/src/day1/변수문제.java
@@ -0,0 +1,57 @@
+package day1;
+
+public class 변수문제 {
+
+	public static void main(String[] args) {
+		//문제1. 홍길동의 국어점수를 저장하려한다.
+		//이때 필요한 변수를 선언하세요. 단, 정수는
+		//정수로 저장한다.
+		int korScore;
+		
+		//문제 홍길동의 수학점수를 저장하려한다
+		//이때 필요한 변수를 선언하세요 단, 정수는
+		//A, B, C, D, F로 저장한다.
+		char mathsocre;
+		
+		//문제3 다음과 같이 국어(kor),수학(math),
+		//영어(eng) 성적이 있을때, 성적의 합과
+		//평균을 저장할 변수를 선언하세요.
+		
+		int kor = 100;
+		int math = 90;
+		int eng =80;
+		
+		int sum;//정수
+		double averge;//평균
+		
+		/* 문제4 다음중 문제가있는 변수선언은?
+		 *  1. char ch1 = 'A';
+		 *  2. char ch2 = '11';
+		 *  3. char ch3 = '_';
+		 *  4. char ch4 = C;
+		 *  5. char ch5 = "D";
+		 */
+		
+		//*  2. char ch2 = '11';
+		//*  4. char ch4 = C;
+		//*  5. char ch5 = "D"; 
+		
+		//질문 : int 대신 long 쓸수있는대 왜안쓰는가.
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+
+	}
+
+}
